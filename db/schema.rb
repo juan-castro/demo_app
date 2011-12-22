ed9aaccd60a3aaaf7ad7dfd3e062482e1db6497d
Juan Castro <j.castro@tukaiz.com>
2011-12-22 11:37:37 -0600

    Added the new fields for the Micropost

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222173004) do
+ActiveRecord::Schema.define(:version => 20111222173641) do
 
   create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
